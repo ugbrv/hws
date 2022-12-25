d6468133ae98c2485d4d1aec1afc9bef40ae2483
ugbrv <114352320+ugbrv@users.noreply.github.com>
2022-12-25 14:54:27 +0500

    Update ConsoleApp1/ConsoleApp1/Program.cs
    Co-authored-by: Belov Alexander <56396003+Belov-AI@users.noreply.github.com>

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Введите 1-й катет: ");
             double a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите 2 катет: ");
+            Console.WriteLine("Введите 2-й катет: ");
             double b = int.Parse(Console.ReadLine());
             double S = (a + b) / 2;
             double c = Math.Sqrt((a * a) + (b * b));
